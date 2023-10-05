6157d5d9d5206674dae3d61f216a777b439d181d
dkamundi <davekamundi@gmail.com>
2023-10-05 21:48:02 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""returns float of an arguement"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Get the floor of a float.
+
+    Args:
+        n (float): The float for which the floor value is needed.
+
+    Returns:
+        int: The floor value of the float `n`.
+    """
+    return math.floor(n)
