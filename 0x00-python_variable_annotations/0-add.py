917410a8dfcf474100d149d1001a175e26936b46
dkamundi <davekamundi@gmail.com>
2023-10-05 21:30:56 +0000

    0. Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-"""
-This module provides functions for basic arithmetic operations.
-"""
+"""This module provides functions for basic arithmetic operations."""
 
 def add(a: float, b: float) -> float:
-
+    """
+    returns sum of two values
+    Args::
+        a(float)
+        b(float)
+    """
     return a + b
 
