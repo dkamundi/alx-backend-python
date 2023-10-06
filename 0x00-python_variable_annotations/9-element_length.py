da5c5f1df7671a23996cd68e68b66b5f0c1074c3
dkamundi <davekamundi@gmail.com>
2023-10-06 02:01:28 +0000

    9. Let's duck type an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 """Annonating the following
 
-def element_length(lst):
-  return [(i, len(i)) for i in lst]
+
+>>>def element_length(lst):
+>>>  return [(i, len(i)) for i in lst]
 """
-
-
 from typing import List, Tuple
 
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
     Calculate the length of each element in the input list.
-
-    Args:
-        lst (List[str]): The list of strings for which the length of each element is calculated.
-
-    Returns:
-        List[Tuple[str, int]]: A list of tuples where each tuple contains a string from the input
-        list and its corresponding length as an integer.
     """
     return [(i, len(i)) for i in lst]
